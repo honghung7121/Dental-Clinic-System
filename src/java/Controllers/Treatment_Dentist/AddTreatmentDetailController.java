0f844a6bf679f7d3749c3c6a359e7b551d60893c
Robert-Tri <ductri3122002@gmail.com>
2023-06-17 20:47:55 +0700

    fix name DAO

--- src/java/Controllers/Treatment_Dentist/AddTreatmentDetailController.java
+++ src/java/Controllers/Treatment_Dentist/AddTreatmentDetailController.java
@@ -56,7 +56,7 @@ public class AddTreatmentDetailController extends HttpServlet {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
             final String formattedDate = localDate.format(formatter);
                 
-            int idPatient = TreatmentCourseDetailDAO.getIDPatientByDetailID(idTreatment);
+            int idPatient = TreatmentCourseDetailDAO.getIDPatientByTreatmentID(idTreatment);
             final User patient = UserDAO.getPatient(idPatient);
             if (check) {
                 Runnable myRunnable = new Runnable() {
